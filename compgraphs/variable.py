9fa217d33c59d169b7736a669ba8060b7818b0a1
Miles McCain <0@rmrm.io>
2018-10-01 16:06:34 -0400

    Add file and function docstrings

--- compgraphs/variable.py
+++ compgraphs/variable.py
@@ -1,3 +1,12 @@
+"""
+This file defines the Variable, a class used for basic mathematical operations
+and gradient calculations.
+
+Authors: MILES MCCAIN and LIV MARTENS
+License: GPLv3
+"""
+
+
 import random
 import numpy as np
 import math
@@ -13,7 +22,11 @@ class Variable():
         self.name = name
 
     def name(self, name):
-        """Name the variable for pretty printing. Does not affect `eval_`"""
+        """Name the variable for pretty printing. Doesn't affect `eval_`.
+        
+        Arguments:
+            name {string} -- the name of the variable
+        """
         self.name = name
 
     def __repr__(self):
@@ -27,10 +40,36 @@ class Variable():
         return self.identifier
 
     def eval_(self, values):
+        """Evaluate the variable with the given values.
+        
+        Arguments:
+            values {dictionary} -- a dictionary of values, where the
+            keys are variable objects and their values are floats
+        
+        Returns:
+            float -- the value of the evaluated variable
+        """
         return values[self]
 
     def ranged_eval(self, values, min=None, max=None, precondition=None):
-        """Exclusive ranged evaluation, with optional precondition"""
+        """Perform a ranged evaluation of the variable on the given values.
+        This method allows a range to be specified as well as a precondition,
+        which helps to prevent floating point rounding errors.
+        
+        Arguments:
+            values {dictionary} -- a dictionary of the values (see `eval_`)
+        
+        Keyword Arguments:
+            min {float} -- the miniumum output value (not enforced if None) (default: {None})
+            max {float} -- the maxiumum output value (not enforced if None) (default: {None})
+            precondition {function} -- a function that returns true or false on the pre-ranged value (default: {None})
+        
+        Raises:
+            Exception -- if the precondition fails
+        
+        Returns:
+            float -- the evaluated value
+        """
         value = self.eval_(values)
         if precondition != None:
             if not precondition(value):
@@ -42,6 +81,16 @@ class Variable():
         return value
 
     def grad(self, values):
+        """Calculate the gradient at any given set of values.
+        
+        Arguments:
+            values {dictionary} -- the values of the variable
+            (only include the required values!)
+        
+        Returns:
+            np.array -- the gradient vector
+        """
+
         self_location = self.order(values)
         pre_self = self_location
         post_self = len(values) - 1 - self_location
@@ -50,6 +99,15 @@ class Variable():
 
     @staticmethod
     def exp(var):
+        """Exponentiate the variable (e ** var)
+        
+        Arguments:
+            var {Variable, float, int} -- the variable to exponentiate
+        
+        Returns:
+            Variable -- a variable that has been exponentiated
+        """
+
         if isinstance(var, Variable):
             return Variable(eval_=lambda values: math.e ** var.eval_(values),
                             grad=lambda values: (math.e ** var.eval_(values))*var.grad(values),
@@ -59,6 +117,15 @@ class Variable():
 
     @staticmethod
     def log(var):
+        """Take the logarithm (base e) of the given variable.
+        
+        Arguments:
+            var {Variable, float, int} -- the variable to take the logarithm of
+        
+        Returns:
+            Variable -- a variable that represents the log of the given variable
+        """
+
         if isinstance(var, Variable):
             return Variable(eval_=lambda values: math.log(var.ranged_eval(values, min=0, precondition=lambda k: k >= 0)),
                             # the precondition for eval_ deals with floating point rounding errors while still showing errors
@@ -68,6 +135,15 @@ class Variable():
             return math.log(var)
 
     def __add__(self, other):
+        """Add two variables together.
+        
+        Arguments:
+            other {Variable, float, int} -- the variable to add
+        
+        Returns:
+            Variable -- the added variables
+        """
+
         if isinstance(other, Variable):
             return Variable(eval_=lambda values: self.eval_(values) + other.eval_(values),
                             grad=lambda values: self.grad(values) + other.grad(values),
@@ -81,12 +157,30 @@ class Variable():
         return self + other
 
     def __sub__(self, other):
+        """Subtract a variable from another.
+        
+        Arguments:
+            other {Variable, float, int} -- the variable to subtract
+        
+        Returns:
+            Variable -- the result of the subtraction operation
+        """
+
         return self + (other * -1)
 
     def __rsub__(self, other):
         return self * -1 + other
 
     def __mul__(self, other):
+        """Multiply two variables together.
+        
+        Arguments:
+            other {Variable, float, int} -- the variable to multiply by
+        
+        Returns:
+            Variable -- the result of the multiplication operation
+        """
+
         if isinstance(other, Variable):
             return Variable(eval_=lambda values: self.eval_(values) * other.eval_(values),
                             grad=lambda values: self.grad(values)*other.eval_(values) + other.grad(values)*self.eval_(values),
@@ -100,6 +194,15 @@ class Variable():
         return self * other
 
     def __pow__(self, other):
+        """Raise a variable to the power of a _constant_.
+        
+        Arguments:
+            other {float, int} -- the value to raise the variable to
+        
+        Returns:
+            Variable -- the variable raised to the given power
+        """
+
         if isinstance(other, Variable):
             # logarithmic differentiation? TODO
             return NotImplemented
@@ -111,6 +214,15 @@ class Variable():
     # __rpow__ not implemented; we simply don't have the rules for it
 
     def __truediv__(self, other):
+        """Divide a variable by another.
+        
+        Arguments:
+            other {Variable, float, int} -- the denominator value/variable
+        
+        Returns:
+            Variable -- a variable of the result of the division operation
+        """
+
         reciprocal = other ** -1
         return self * reciprocal
 
@@ -120,6 +232,15 @@ class Variable():
 
     def order(self, values):
         """Returns the order of the variable in the given list of values,
-        as it would be returned by the gradient function, for example."""
+        as it would be returned by the gradient function, for example.
+        
+        Arguments:
+            values {dictionary} -- the dictionary of values
+        
+        Returns:
+            int -- the index of the variable within the dictionary (and in,
+            for example, a gradient vector)
+        """
+
         order = sorted([hash(key) for key in values.keys()])
         return order.index(hash(self))
